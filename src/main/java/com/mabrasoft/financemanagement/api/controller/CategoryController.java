94e2f4c0aa346438409d892db38f2a9f5713e193
Miriã Fassarella <miriafassarella@gmail.com>
2023-08-30 22:28:42 -0400

    Adding category using save method

--- src/main/java/com/mabrasoft/financemanagement/api/controller/CategoryController.java
+++ src/main/java/com/mabrasoft/financemanagement/api/controller/CategoryController.java
@@ -7,10 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mabrasoft.financemanagement.domain.exceptions.EntityNotFoundException;
 import com.mabrasoft.financemanagement.domain.model.Category;
 import com.mabrasoft.financemanagement.domain.service.CategoryService;
 
@@ -28,9 +31,19 @@ public class CategoryController {
 	}
 	
 	@GetMapping("/{categoryId}")
-	public ResponseEntity<Category> categorySearch(@PathVariable Long categoryId){
-		Category category = categoryService.search(categoryId);
-		return ResponseEntity.status(HttpStatus.FOUND).body(category);
+	public ResponseEntity<?> categorySearch(@PathVariable Long categoryId){
+		try {
+			Category category = categoryService.search(categoryId);
+			return ResponseEntity.status(HttpStatus.FOUND).body(category);
+		}catch(EntityNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> categoryAdd(@RequestBody Category category){
+		category = categoryService.add(category);
+		return ResponseEntity.status(HttpStatus.CREATED).body(category);
 	}
 
 }
