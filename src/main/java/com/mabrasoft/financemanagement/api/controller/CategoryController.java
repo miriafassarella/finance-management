eaa15a1b508b2fda3169d90213fb596ff6a36bb6
Miriã Fassarella <miriafassarella@gmail.com>
2023-08-30 20:03:49 -0400

    Find a category with findById

--- src/main/java/com/mabrasoft/financemanagement/api/controller/CategoryController.java
+++ src/main/java/com/mabrasoft/financemanagement/api/controller/CategoryController.java
@@ -3,8 +3,12 @@ package com.mabrasoft.financemanagement.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mabrasoft.financemanagement.domain.model.Category;
@@ -18,8 +22,15 @@ public class CategoryController {
 	CategoryService categoryService;
 	
 	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
 	public List<Category> categoryList(){
 		return categoryService.list();
+	}
+	
+	@GetMapping("/{categoryId}")
+	public ResponseEntity<Category> categorySearch(@PathVariable Long categoryId){
+		Category category = categoryService.search(categoryId);
+		return ResponseEntity.status(HttpStatus.FOUND).body(category);
 	}
 
 }
--- src/main/java/com/mabrasoft/financemanagement/domain/service/CategoryService.java
+++ src/main/java/com/mabrasoft/financemanagement/domain/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.mabrasoft.financemanagement.domain.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,11 @@ public class CategoryService {
 	
 	public List<Category> list(){
 		return categoryRepository.findAll();
+	}
+	
+	public Category search(Long categoryId) {
+		Optional<Category> category =  categoryRepository.findById(categoryId);
+		return category.get();
 	}
 
 }
