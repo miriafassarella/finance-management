0d893bb87dc5bde8a325724da0c468ba283e1268
Miriã Fassarella <miriafassarella@gmail.com>
2023-09-29 10:48:47 -0400

    Dynamic query with JPQL

--- src/main/java/com/mabrasoft/financemanagement/infrastructure/repository/EntryRepositoryImpl.java
+++ src/main/java/com/mabrasoft/financemanagement/infrastructure/repository/EntryRepositoryImpl.java
@@ -1,15 +1,18 @@
 package com.mabrasoft.financemanagement.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.mabrasoft.financemanagement.domain.model.Entry;
 import com.mabrasoft.financemanagement.domain.repository.QueriesEntryRepository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 @Repository
 public class EntryRepositoryImpl implements QueriesEntryRepository{
@@ -19,15 +22,32 @@ public class EntryRepositoryImpl implements QueriesEntryRepository{
 
 	@Override
 	public List<Entry> find(String description,
-			BigDecimal priceInitial, BigDecimal priceFinal){
+			BigDecimal priceInitialEntry, BigDecimal priceFinalEntry){
 		
-		var jpql = "from Entry where description like :description "
-		+ "and price between :priceInitial and :priceFinal";
 		
-		return manager.createQuery(jpql, Entry.class)
-				.setParameter("description", "%" + description + "%")
-				.setParameter("priceInitial", priceInitial)
-				.setParameter("priceFinal", priceFinal)
-				.getResultList();
-	};
+		
+		var jpql = new StringBuilder();
+		jpql.append("from Entry where 0 = 0 ");
+		
+		var parameters = new HashMap<String, Object>();
+		
+		if(StringUtils.hasLength(description)) { /*Checking if it is not empty or if it is not null*/
+			jpql.append("and description like :description ");
+			parameters.put("description", "%" + description + "%");
+		}
+		if(priceInitialEntry != null) {
+			jpql.append("and price >= :priceInitial ");
+			parameters.put("priceInitial", priceInitialEntry);
+		}
+		if(priceFinalEntry != null) {
+			jpql.append("and price <= :priceFinal ");
+			parameters.put("priceFinal", priceFinalEntry);
+		}
+		
+		TypedQuery<Entry> query =  manager.createQuery(jpql.toString(), Entry.class); 
+		
+		parameters.forEach((key, value) -> query.setParameter(key, value));
+				
+		return query.getResultList();
+	}
 }
