621bd28abbe0c46c40eb11a6b30a0bc3585d76ad
Miriã Fassarella <miriafassarella@gmail.com>
2023-10-23 06:23:17 -0400

    Returning handleExceptionInternal

--- src/main/java/com/mabrasoft/financemanagement/exceptionhendler/FinanceExceptionHandler.java
+++ src/main/java/com/mabrasoft/financemanagement/exceptionhendler/FinanceExceptionHandler.java
@@ -2,19 +2,13 @@ package com.mabrasoft.financemanagement.exceptionhendler;
 
 
 import java.time.LocalDateTime;
-import java.util.NoSuchElementException;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
+
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,34 +20,24 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice //it observes the entire application
 public class FinanceExceptionHandler extends ResponseEntityExceptionHandler {
 
-	/*
-	 * @Autowired private MessageSource messageSource;
-	 * 
-	 * @Override protected ResponseEntity<Object>
-	 * handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders
-	 * headers, HttpStatusCode status, WebRequest request) { String developerMessage
-	 * = ex.getCause().toString(); String userMessage =
-	 * messageSource.getMessage("message.invalid", null,
-	 * LocaleContextHolder.getLocale()); Error error = new Error(userMessage,
-	 * developerMessage); return handleExceptionInternal(ex, error, headers,
-	 * HttpStatus.BAD_REQUEST, request); }
-	 * 
-	 * @ExceptionHandler({NoSuchElementException.class}) public
-	 * ResponseEntity<Object> handleEmptyResultDataException(NoSuchElementException
-	 * ex, WebRequest request) { String developerMessage = ex.toString(); String
-	 * userMessage = messageSource.getMessage("resource.not-found", null,
-	 * LocaleContextHolder.getLocale()); Error error = new Error(userMessage,
-	 * developerMessage); return handleExceptionInternal(ex, error, new
-	 * HttpHeaders(), HttpStatus.NOT_FOUND, request); }
-	 */
+	
 		
 		@ExceptionHandler({DataIntegrityViolationException.class})
-		public ResponseEntity<?> treatDataIntegrity(DataIntegrityViolationException e){
-			Error error = Error.builder()
-					.dateHour(LocalDateTime.now())
-					.message(e.getMessage()).build();
+		public ResponseEntity<?> treatDataIntegrity(DataIntegrityViolationException ex, WebRequest request){
+			return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 			
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
+		
+		@Override
+		protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatusCode statusCode, WebRequest request) {
+			
+			body = Error.builder()
+					.dateHour(LocalDateTime.now())
+					.message(ex.getMessage())
+					.build();
+		
+		return super.handleExceptionInternal(ex, body, headers, statusCode, request);
 		}
 			
 	}
